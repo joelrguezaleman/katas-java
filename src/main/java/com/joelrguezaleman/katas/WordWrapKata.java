3013557c60a888406bcdf7f3a82dfb8da636013c
Joel <contact@joelrodriguezaleman.net>
2018-03-24 17:57:54 +0100

    WordWrapKata TDD Iteration: throw an exception if the column number is 0 or lower

--- src/main/java/com/joelrguezaleman/katas/WordWrapKata.java
+++ src/main/java/com/joelrguezaleman/katas/WordWrapKata.java
@@ -2,4 +2,8 @@ package com.joelrguezaleman.katas;
 
 public class WordWrapKata
 {
+    public static String wrap(String text, int columnNumber) throws Exception
+    {
+        throw new Exception();
+    }
 }
--- src/test/java/com/joelrguezaleman/katas/WordWrapKataTest.java
+++ src/test/java/com/joelrguezaleman/katas/WordWrapKataTest.java
@@ -1,5 +1,30 @@
 package com.joelrguezaleman.katas;
 
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 public class WordWrapKataTest
 {
+    @ParameterizedTest
+    @MethodSource("invalidColumnNumbersProvider")
+    public void itThrowsAnExceptionWhenTheColumnNumberIs0OrLower(int columnNumber)
+    {
+        assertThrows(
+            Exception.class,
+            () -> {
+                WordWrapKata.wrap("", columnNumber);
+            }
+        );
+    }
+
+    private static Stream<Arguments> invalidColumnNumbersProvider()
+    {
+        return Stream.of(
+            Arguments.of(0),
+            Arguments.of(-1)
+        );
+    }
 }
