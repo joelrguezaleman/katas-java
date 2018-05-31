3d46199803ac933d3153ad3ab4d5cdf22ef034bb
Joel Rodríguez Alemán <contact@joelrodriguezaleman.net>
2018-05-31 21:37:10 +0200

    MarsRoverKata Implement command to turn the MarsRover right

--- src/main/java/com/joelrguezaleman/katas/MarsRover/Command/TurnRightCommand.java
+++ src/main/java/com/joelrguezaleman/katas/MarsRover/Command/TurnRightCommand.java
@@ -4,5 +4,14 @@ public class TurnRightCommand implements Command
 {
     public void run(Position position)
     {
+        if (position.isSouth()) {
+            position.setDirection(Directions.WEST);
+        } else if (position.isWest()) {
+            position.setDirection(Directions.NORTH);
+        } else if (position.isNorth()) {
+            position.setDirection(Directions.EAST);
+        } else {
+            position.setDirection(Directions.SOUTH);
+        }
     }
 }
