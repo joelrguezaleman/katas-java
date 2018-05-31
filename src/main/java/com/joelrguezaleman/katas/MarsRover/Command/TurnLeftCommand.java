85e67f7f4f89d9de6d3394ce87dd8e0b19387fcc
Joel Rodríguez Alemán <contact@joelrodriguezaleman.net>
2018-05-31 21:34:55 +0200

    MarsRoverKata Implement command to turn the MarsRover left

--- src/main/java/com/joelrguezaleman/katas/MarsRover/Command/TurnLeftCommand.java
+++ src/main/java/com/joelrguezaleman/katas/MarsRover/Command/TurnLeftCommand.java
@@ -4,5 +4,14 @@ public class TurnLeftCommand implements Command
 {
     public void run(Position position)
     {
+        if (position.isSouth()) {
+            position.setDirection(Directions.EAST);
+        } else if (position.isWest()) {
+            position.setDirection(Directions.SOUTH);
+        } else if (position.isNorth()) {
+            position.setDirection(Directions.WEST);
+        } else {
+            position.setDirection(Directions.NORTH);
+        }
     }
 }
---
+++ src/test/java/com/joelrguezaleman/katas/MarsRover/TurnLeftCommandTest.java
@@ -0,0 +1,50 @@
+package com.joelrguezaleman.katas.marsrover;
+
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class TurnLeftCommandTest
+{
+    @ParameterizedTest
+    @MethodSource("positionProvider")
+    public void itUpdatesThePositionInABackwardDirection(
+        Position position,
+        Position expectedPosition
+    ) {
+        TurnLeftCommand command = new TurnLeftCommand();
+
+        command.run(position);
+
+        assertEquals(expectedPosition.x(), position.x());
+        assertEquals(expectedPosition.y(), position.y());
+        assertEquals(expectedPosition.isNorth(), position.isNorth());
+        assertEquals(expectedPosition.isEast(), position.isEast());
+        assertEquals(expectedPosition.isSouth(), position.isSouth());
+        assertEquals(expectedPosition.isWest(), position.isWest());
+    }
+
+    private static Stream<Arguments> positionProvider() throws InvalidPositionException
+    {
+        return Stream.of(
+            Arguments.of(
+                new Position(0, 0, Directions.EAST),
+                new Position(0, 0, Directions.NORTH)
+            ),
+            Arguments.of(
+                new Position(0, 0, Directions.NORTH),
+                new Position(0, 0, Directions.WEST)
+            ),
+            Arguments.of(
+                new Position(0, 0, Directions.WEST),
+                new Position(0, 0, Directions.SOUTH)
+            ),
+            Arguments.of(
+                new Position(0, 0, Directions.SOUTH),
+                new Position(0, 0, Directions.EAST)
+            )
+        );
+    }
+}
