7be8a8c1c08c1f7fc8550c245dc3fcf624a475fd
Joel Rodríguez Alemán <contact@joelrodriguezaleman.net>
2018-05-31 21:28:03 +0200

    MarsRoverKata Implement command to move the MarsRover backward

--- src/main/java/com/joelrguezaleman/katas/MarsRover/Command/MoveBackwardCommand.java
+++ src/main/java/com/joelrguezaleman/katas/MarsRover/Command/MoveBackwardCommand.java
@@ -4,5 +4,14 @@ public class MoveBackwardCommand implements Command
 {
     public void run(Position position)
     {
+        if (position.isSouth()) {
+            position.increaseY();
+        } else if (position.isWest()) {
+            position.increaseX();
+        } else if (position.isNorth()) {
+            position.decreaseY();
+        } else {
+            position.decreaseX();
+        }
     }
 }
